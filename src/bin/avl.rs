afea1e7979e6cc53e2aa66982af4f59a2ed83dc0
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2025-03-09 11:54:11 +0900

    balance

--- src/bin/avl.rs
+++ src/bin/avl.rs
@@ -26,18 +26,18 @@ impl AVLTreeSet {
 
     pub fn insert(&mut self, value: i32) -> bool {
         fn insert(node: &mut Option<NonNull<Node>>, value: i32) -> bool {
-            if let Some(node) = node {
-                match value.cmp(&unsafe { node.as_ref() }.value) {
+            if let Some(node) = node.map(|mut node| unsafe { node.as_mut() }) {
+                match value.cmp(&node.value) {
                     Ordering::Equal => {
                         return false;
                     }
                     Ordering::Less => {
-                        if !insert(&mut unsafe { node.as_mut() }.left, value) {
+                        if !insert(&mut node.left, value) {
                             return false;
                         }
                     }
                     Ordering::Greater => {
-                        if !insert(&mut unsafe { node.as_mut() }.right, value) {
+                        if !insert(&mut node.right, value) {
                             return false;
                         }
                     }
@@ -45,17 +45,15 @@ impl AVLTreeSet {
             } else {
                 *node = Some(Node::new(value));
             }
-            // balance(node);
+            Node::balance(node);
             true
         }
-
         insert(&mut self.root, value)
     }
 
     pub fn contains(&mut self, value: &i32) -> bool {
         let mut p = &self.root;
-        while let Some(node) = p {
-            let node = unsafe { node.as_ref() };
+        while let Some(node) = p.map(|node| unsafe { node.as_ref() }) {
             match value.cmp(&node.value) {
                 Ordering::Equal => {
                     return true;
@@ -72,7 +70,48 @@ impl AVLTreeSet {
     }
 
     pub fn remove(&mut self, value: &i32) -> bool {
-        todo!()
+        fn remove(node: &mut Option<NonNull<Node>>, value: &i32) -> bool {
+            if let Some(x) = node {
+                let x = unsafe { x.as_mut() };
+
+                match value.cmp(&x.value) {
+                    Ordering::Equal => {
+                        if x.left.is_none() {
+                            *node = x.right;
+                            return true;
+                        } else if x.right.is_none() {
+                            *node = x.left;
+                            return true;
+                        } else {
+                            let mut right = x.right.unwrap();
+                            while let Some(left) = unsafe { right.as_ref().left } {
+                                right = left;
+                            }
+                            x.value = unsafe { right.as_ref().value };
+                            if remove(&mut x.right, &unsafe { right.as_ref().value }) {
+                                Node::balance(node);
+                                return true;
+                            }
+                        }
+                    }
+                    Ordering::Less => {
+                        if remove(&mut x.left, value) {
+                            Node::balance(node);
+                            return true;
+                        }
+                    }
+                    Ordering::Greater => {
+                        if remove(&mut x.right, value) {
+                            Node::balance(node);
+                            return true;
+                        }
+                    }
+                }
+            }
+            false
+        }
+
+        remove(&mut self.root, value)
     }
 
     /// 昇順でk番目の要素を取得する
@@ -88,9 +127,8 @@ impl AVLTreeSet {
     pub fn iter(&self) -> impl Iterator<Item = &i32> + '_ {
         // 再帰的に要素を収集する
         // 結果は昇順になる
-        fn collect(root: &Option<NonNull<Node>>, v: &mut Vec<&i32>) {
-            if let Some(node) = root {
-                let node = unsafe { node.as_ref() };
+        fn collect(node: &Option<NonNull<Node>>, v: &mut Vec<&i32>) {
+            if let Some(node) = node.map(|node| unsafe { node.as_ref() }) {
                 collect(&node.left, v);
                 v.push(&node.value);
                 collect(&node.right, v);
@@ -111,8 +149,8 @@ impl Default for AVLTreeSet {
 impl Drop for AVLTreeSet {
     fn drop(&mut self) {
         // 再帰的にメモリを解放する
-        fn free(root: &mut Option<NonNull<Node>>) {
-            if let Some(mut node) = root.take() {
+        fn free(node: &mut Option<NonNull<Node>>) {
+            if let Some(mut node) = node.take() {
                 free(&mut unsafe { node.as_mut() }.left);
                 free(&mut unsafe { node.as_mut() }.right);
                 unsafe { drop(Box::from_raw(node.as_ptr())) };
@@ -144,14 +182,14 @@ impl<const N: usize> From<[i32; N]> for AVLTreeSet {
 
 impl Debug for AVLTreeSet {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_list().entries(self.iter()).finish()
+        f.debug_set().entries(self.iter()).finish()
     }
 }
 
 struct Node {
     value: i32,
     len: usize,
-    height: usize,
+    height: i32,
     left: Option<NonNull<Node>>,
     right: Option<NonNull<Node>>,
 }
@@ -177,131 +215,162 @@ impl Node {
         node.map_or(0, |node| unsafe { node.as_ref() }.len)
     }
 
-    fn height(node: &Option<NonNull<Node>>) -> usize {
+    fn height(node: &Option<NonNull<Node>>) -> i32 {
         node.map_or(0, |node| unsafe { node.as_ref() }.height)
     }
-}
 
-// rootを根とする部分木を平衡にする
-fn balance(node: &mut Option<NonNull<Node>>) {
-    if let Some(node) = node.as_mut() {
-        let left_height = Node::height(&unsafe { node.as_ref() }.left);
-        let right_height = Node::height(&unsafe { node.as_ref() }.right);
-        if left_height > right_height + 1 {
-            // 左部分木が高い場合
+    /// 平衡
+    fn balance(node: &mut Option<NonNull<Node>>) {
+        /// 左部分木と右部分木の高さの差
+        /// 左部分木の高さ - 右部分木の高さ
+        #[inline]
+        fn diff_height(node: &NonNull<Node>) -> i32 {
+            let node = unsafe { node.as_ref() };
+            Node::height(&node.left) - Node::height(&node.right)
+        }
 
-            // 二重回転
-            let left = unsafe { node.as_ref().left.unwrap().as_ref() };
-            if Node::height(&left.left) < Node::height(&left.right) {
-                // rotate_left(&mut unsafe { node.as_mut().left });
+        /// 木を右回転させる
+        fn rotate_right(root: &mut Option<NonNull<Node>>) {
+            if let Some(x) = root {
+                let mut y = unsafe { x.as_mut() }.left.unwrap();
+                unsafe { x.as_mut() }.left = unsafe { y.as_mut() }.right;
+                unsafe { x.as_mut() }.update();
+                unsafe { y.as_mut() }.right = Some(*x);
+                unsafe { y.as_mut() }.update();
+                *root = Some(y);
             }
+        }
 
-            rotate_right(node);
-        } else if right_height > left_height + 1 {
-            // 右部分木が高い場合
-
-            // 二重回転
-            let right = unsafe { node.as_ref().right.unwrap().as_ref() };
-            if Node::height(&right.right) < Node::height(&right.left) {
-                rotate_right(&mut unsafe { node.as_mut().right.unwrap() });
+        /// 木を左回転させる
+        fn rotate_left(root: &mut Option<NonNull<Node>>) {
+            if let Some(x) = root {
+                let mut y = unsafe { x.as_mut() }.right.unwrap();
+                unsafe { x.as_mut() }.right = unsafe { y.as_mut() }.left;
+                unsafe { x.as_mut() }.update();
+                unsafe { y.as_mut() }.left = Some(*x);
+                unsafe { y.as_mut() }.update();
+                *root = Some(y);
             }
+        }
 
-            // rotate_left(node);
-        } else {
-            // すでに平衡である場合
-            unsafe { node.as_mut() }.update();
+        if let Some(x) = node {
+            let d = diff_height(&x);
+            let x = unsafe { x.as_mut() };
+
+            if d > 1 {
+                // 左部分木が高い場合
+
+                if diff_height(&x.left.unwrap()) < 0 {
+                    rotate_left(&mut x.left);
+                }
+
+                rotate_right(node);
+            } else if d < -1 {
+                // 右部分木が高い場合
+
+                if diff_height(&x.right.unwrap()) > 0 {
+                    rotate_right(&mut x.right);
+                }
+
+                rotate_left(node);
+            } else {
+                x.update();
+            }
         }
     }
 }
 
-/// 木を右回転させる
-/// 回転の方向は[Wikipedia](https://ja.wikipedia.org/wiki/%E6%9C%A8%E3%81%AE%E5%9B%9E%E8%BB%A2)準拠
-fn rotate_right(node: &mut NonNull<Node>) {
-    //TODO:
-    unsafe {
-        let p = node.as_mut();
-        assert!(p.left.is_some());
-        let left = p.left.unwrap();
-        p.left = p.right;
-        p.right = p.left.unwrap().as_mut().right;
-        // p.left.unwrap().as_mut().right = Some(*node);
-        // p.left.unwrap().as_mut().update();
-        // *node = left;
-        // node.as_mut().update();
-    }
-}
-
-/// 木を左回転させる
-/// 回転の方向は[Wikipedia](https://ja.wikipedia.org/wiki/%E6%9C%A8%E3%81%AE%E5%9B%9E%E8%BB%A2)準拠
-// fn rotate_left(node: &mut Option<NonNull<Node>>) {
-//     unsafe {
-//         assert!(node.is_some());
-//         let p = node.take().unwrap().as_mut();
-//         assert!(p.right.is_some());
-//         let right = p.right.take().unwrap();
-//         p.right = p.left;
-//         p.left = p.right.unwrap().as_mut().left;
-//         p.right.unwrap().as_mut().left = *node;
-//         p.right.unwrap().as_mut().update();
-//         *node = Some(right);
-//         node.unwrap().as_mut().update();
-//     }
-// }
-
+#[cfg(debug_assertions)]
 #[allow(unused)]
-fn visualize(tree: &AVLTreeSet) -> String {
-    fn visualize(
-        node: &Option<NonNull<Node>>,
-        prefix: &str,
-        is_root: bool,
-        is_last: bool,
-        res: &mut String,
-    ) {
-        if let Some(node) = node.map(|node| unsafe { node.as_ref() }) {
-            if is_root {
-                *res += &format!("{}\n", node.value);
-            } else {
-                *res += &format!(
-                    "{}{}{}\n",
-                    prefix,
-                    if is_last { "└── " } else { "├── " },
-                    node.value
-                );
+impl AVLTreeSet {
+    fn visualize(&self) -> String {
+        fn visualize(
+            node: &Option<NonNull<Node>>,
+            prefix: &str,
+            is_root: bool,
+            is_last: bool,
+            res: &mut String,
+        ) {
+            if let Some(node) = node.map(|node| unsafe { node.as_ref() }) {
+                if is_root {
+                    *res += &format!("{}\n", node.value);
+                } else {
+                    *res += &format!(
+                        "{}{}{}\n",
+                        prefix,
+                        if is_last { "└── " } else { "├── " },
+                        node.value
+                    );
+                }
+
+                let new_prefix = if is_root {
+                    String::new()
+                } else {
+                    format!("{}{}", prefix, if is_last { "    " } else { "│   " })
+                };
+
+                visualize(&node.right, &new_prefix, false, node.left.is_none(), res);
+                visualize(&node.left, &new_prefix, false, true, res);
             }
-
-            let new_prefix = if is_root {
-                String::new()
-            } else {
-                format!("{}{}", prefix, if is_last { "    " } else { "│   " })
-            };
-
-            visualize(&node.right, &new_prefix, false, node.left.is_none(), res);
-            visualize(&node.left, &new_prefix, false, true, res);
         }
-    }
 
-    let mut res = String::new();
-    visualize(&tree.root, "", true, true, &mut res);
-    res
+        let mut res = String::new();
+        visualize(&self.root, "", true, true, &mut res);
+        res
+    }
 }
 
 #[allow(unused)]
 fn main() {
+    use rand::{rng, Rng};
     use std::collections::BTreeSet;
+    let mut rng = rng();
 
     println!("AVLTreeSet");
 
-    let tree = AVLTreeSet::from([1, 2, 3, 4, 5]);
-    println!("{:?}", tree);
-    println!("{}", visualize(&tree));
+    // let tree = AVLTreeSet::from([1, 2, 3, 4, 5]);
+    // println!("{:?}", tree);
+    // println!("{}", tree.visualize());
 
-    let tree = AVLTreeSet::from([3, 1, 4, 1, 5, 9, 2]);
-    println!("{:?}", tree);
-    println!("{}", visualize(&tree));
+    // let tree = AVLTreeSet::from([3, 1, 4, 1, 5, 9, 2]);
+    // println!("{:?}", tree);
+    // println!("{}", tree.visualize());
 
-    let mut tree = AVLTreeSet::from([100, 50, 20, 10, 80, 90, 60, 95]);
-    println!("{:?}", tree);
-    println!("{}", visualize(&tree));
+    // let mut tree = AVLTreeSet::from([100, 50, 20, 10, 80, 90, 60, 95]);
+    // println!("{:?}", tree);
+    // println!("{}", tree.visualize());
+
+    // let mut avl = AVLTreeSet::new();
+    // for _ in 0..100 {
+    //     avl.insert(rng.random_range(0..100));
+    // }
+    // println!("{}", avl.visualize());
+
+    let mut avl = AVLTreeSet::new();
+    let mut b = BTreeSet::new();
+    for _ in 0..200 {
+        // 0: insert
+        // 1: contains
+        // 2: remove
+        let t = rng.random_range(0..3);
+        let x = rng.random_range(0..100);
+        match t {
+            0 => {
+                assert_eq!(b.insert(x), avl.insert(x));
+            }
+            1 => {
+                assert_eq!(b.contains(&x), avl.contains(&x));
+            }
+            2 => {
+                assert_eq!(b.remove(&x), avl.remove(&x));
+            }
+            _ => {}
+        }
+        assert_eq!(b.len(), avl.len());
+        assert!(avl.iter().eq(b.iter()));
+    }
+
+    println!("{}", avl.visualize());
 }
 
 #[allow(unused)]
@@ -330,6 +399,15 @@ mod tests {
     }
 
     #[test]
+    fn test_remove() {
+        let mut tree = AVLTreeSet::from([3, 1, 4, 1, 5]);
+        assert!(!tree.remove(&10));
+        assert!(tree.contains(&3));
+        assert!(tree.remove(&3));
+        assert!(!tree.contains(&3));
+    }
+
+    #[test]
     fn test_random() {
         use rand::{rng, Rng};
         use std::collections::BTreeSet;
@@ -344,7 +422,7 @@ mod tests {
                 // 1: contains
                 // 2: remove
                 let t = rng.random_range(0..3);
-                let x = rng.random();
+                let x = rng.random_range(-100..=100);
                 match t {
                     0 => {
                         assert_eq!(b.insert(x), avl.insert(x));
@@ -353,10 +431,11 @@ mod tests {
                         assert_eq!(b.contains(&x), avl.contains(&x));
                     }
                     2 => {
-                        // assert_eq!(b.remove(&x), avl.remove(&x));
+                        assert_eq!(b.remove(&x), avl.remove(&x));
                     }
                     _ => {}
                 }
+                assert_eq!(b.len(), avl.len());
                 assert!(avl.iter().eq(b.iter()));
             }
         }
